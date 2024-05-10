4e15da801b8cebc0c8614ee310c1b828e6fc8c95
wennjiee <1229368677@qq.com>
2024-05-10 14:49:47 +0800

    add hubert for chinese talking

--- data_utils/process.py
+++ data_utils/process.py
@@ -418,7 +418,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('path', type=str, help="path to video file")
     parser.add_argument('--task', type=int, default=-1, help="-1 means all")
-    parser.add_argument('--asr', type=str, default='deepspeech', help="wav2vec or deepspeech or hubert")
+    parser.add_argument('--asr', type=str, default='deepspeech', help="wav2vec or deepspeech or hubert or ave")
 
 
     opt = parser.parse_args()
