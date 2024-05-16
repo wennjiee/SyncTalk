d81400962fa6859feed0227fac1755473c464a46
wennjiee <1229368677@qq.com>
2024-05-16 15:07:34 +0800

    update training params

--- scripts/train.py
+++ scripts/train.py
@@ -1,12 +1,12 @@
 import os
 import glob
 if __name__ == '__main__':
-    train_name = 'wwj_test'
+    train_name = 'wwj_2'
     audio_model = 'hubert'
     trial_params = f'trial_{train_name}_{audio_model}'
     cmd0 = f'python data_utils/process.py data/{train_name}/{train_name}.mp4 --asr {audio_model}'
-    cmd1 = f'python main.py data/{train_name} --workspace model/{trial_params} -O --iters 60000 --asr_model {audio_model}'
-    cmd2 = f'python main.py data/{train_name} --workspace model/{trial_params} -O --iters 120000 --finetune_lips --patch_size 64 --asr_model {audio_model}'
+    cmd1 = f'python main.py data/{train_name} --workspace model/{trial_params} -O --iters 90000 --asr_model {audio_model}'
+    cmd2 = f'python main.py data/{train_name} --workspace model/{trial_params} -O --iters 160000 --finetune_lips --patch_size 64 --asr_model {audio_model}'
     preprocessed_data = f'data/{train_name}/transforms_train.json'
     if not os.path.exists(preprocessed_data):
         os.system(cmd0)
